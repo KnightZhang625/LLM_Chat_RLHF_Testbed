8914eac22b7ac8c48619ee9be22c4a89ff55e06b
Jiaxin Zhang <gvb20207@archie-w.hpc.strath.ac.uk>
2023-08-09 12:38:14 +0100

    add

--- scripts/lora_tuning.sh
+++ scripts/lora_tuning.sh
@@ -7,7 +7,7 @@ CUDA_VISIBLE_DEVICES=0 python lora_tuning.py \
     --lora_rank 4 \
     --per_device_train_batch_size 16 \
     --gradient_accumulation_steps 1 \
-    --num_train_epochs 2 \
+    --num_train_epochs 100 \
     --save_steps 200 \
     --save_total_limit 2 \
     --learning_rate 1e-4 \
